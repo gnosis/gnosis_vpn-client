f127decdeb0dbebfeaac9bdb1b9e051973e637b6
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-03-20 13:31:13 +0100

    work around shellcheck warning

--- docker/monitor.sh
+++ docker/monitor.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -x
 
-declare address="$ADDRESS"
-declare private_key="$PRIVATE_KEY"
-declare server_public_key="$SERVER_PUBLIC_KEY"
-if [ -z "$address" ]; then
+declare ip="$ADDRESS"
+declare priv_key="$PRIVATE_KEY"
+declare server_pub_key="$SERVER_PUBLIC_KEY"
+if [ -z "$ip" ]; then
   echo "ADDRESS is not set"
   exit 1
 fi
-if [ -z "$private_key" ]; then
+if [ -z "$priv_key" ]; then
   echo "PRIVATE_KEY is not set"
   exit 1
 fi
-if [ -z "$server_public_key" ]; then
+if [ -z "$server_pub_key" ]; then
   echo "SERVER_PUBLIC_KEY is not set"
   exit 1
 fi
 
-awk -v cont="$address" '{gsub(/Address = <address>/, "Address = " cont); print}' wgclient.conf > temp.conf && mv temp.conf wgclient.conf
-awk -v cont="$private_key" '{gsub(/PrivateKey = <private key>/, "PrivateKey = " cont); print}' wgclient.conf > temp.conf && mv temp.conf wgclient.conf
-awk -v cont="$server_public_key" '{gsub(/PublicKey = <server public key>/, "PublicKey = " cont); print}' wgclient.conf > temp.conf && mv temp.conf wgclient.conf
+awk -v cont="$ip" '{gsub(/Address = <address>/, "Address = " cont); print}' wgclient.conf > temp.conf && mv temp.conf wgclient.conf
+awk -v cont="$priv_key" '{gsub(/PrivateKey = <private key>/, "PrivateKey = " cont); print}' wgclient.conf > temp.conf && mv temp.conf wgclient.conf
+awk -v cont="$server_pub_key" '{gsub(/PublicKey = <server public key>/, "PublicKey = " cont); print}' wgclient.conf > temp.conf && mv temp.conf wgclient.conf
 
 chmod 600 wgclient.conf
 wg-quick up ./wgclient.conf
