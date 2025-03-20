e46daa4253bd69fa662e9d0c7f50283bc674a641
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-03-20 13:12:23 +0100

    fixed systctl startup call failure

--- docker/monitor.sh
+++ docker/monitor.sh
@@ -23,8 +23,6 @@ awk -v cont="$address" '{gsub(/Address = <address>/, "Address = " cont); print}'
 awk -v cont="$private_key" '{gsub(/PrivateKey = <private key>/, "PrivateKey = " cont); print}' wgclient.conf > temp.conf && mv temp.conf wgclient.conf
 awk -v cont="$server_public_key" '{gsub(/PublicKey = <server public key>/, "PublicKey = " cont); print}' wgclient.conf > temp.conf && mv temp.conf wgclient.conf
 
+chmod 600 wgclient.conf
 wg-quick up ./wgclient.conf
-while true; do
-    wg show wgclient
-    sleep 10
-done
+wait
