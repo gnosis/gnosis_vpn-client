819ea522ca19caa38cb385e4df04056234165697
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-06-27 15:11:00 +0200

    initial talpid-types

---
+++ talpid-types/src/lib.rs
@@ -0,0 +1,15 @@
+#[cfg(target_os = "android")]
+pub mod android;
+pub mod net;
+pub mod tunnel;
+
+#[cfg(target_os = "linux")]
+pub mod cgroup;
+
+#[cfg(target_os = "windows")]
+pub mod split_tunnel;
+
+pub mod drop_guard;
+
+mod error;
+pub use error::*;
---
+++ talpid-types/src/net/obfuscation.rs
@@ -0,0 +1,40 @@
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+use super::{Endpoint, TransportProtocol};
+
+#[derive(Clone, Eq, PartialEq, Deserialize, Serialize, Debug)]
+pub enum ObfuscatorConfig {
+    Udp2Tcp {
+        endpoint: SocketAddr,
+    },
+    Shadowsocks {
+        endpoint: SocketAddr,
+    },
+    Quic {
+        hostname: String,
+        endpoint: SocketAddr,
+    },
+}
+
+impl ObfuscatorConfig {
+    pub fn get_obfuscator_endpoint(&self) -> Endpoint {
+        match self {
+            ObfuscatorConfig::Udp2Tcp { endpoint } => Endpoint {
+                address: *endpoint,
+                protocol: TransportProtocol::Tcp,
+            },
+            ObfuscatorConfig::Shadowsocks { endpoint } => Endpoint {
+                address: *endpoint,
+                protocol: TransportProtocol::Udp,
+            },
+            ObfuscatorConfig::Quic {
+                hostname: _,
+                endpoint,
+            } => Endpoint {
+                address: *endpoint,
+                protocol: TransportProtocol::Udp,
+            },
+        }
+    }
+}
