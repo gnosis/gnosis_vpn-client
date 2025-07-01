a67256cb8fd38ea0cda49e83950363e6bbfad5bd
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-07-01 16:20:18 +0200

    fix new non vendor adjustements

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "Gnosis VPN client service";
 
   inputs = {
-    flake-utils.url = github:numtide/flake-utils;
-    flake-parts.url = github:hercules-ci/flake-parts;
-    nixpkgs.url = github:NixOS/nixpkgs/release-25.05;
-    rust-overlay.url = github:oxalica/rust-overlay/master;
-    crane.url = github:hoprnet/crane/este/find-filter;
-    pre-commit.url = github:cachix/pre-commit-hooks.nix;
-    treefmt-nix.url = github:numtide/treefmt-nix;
-    flake-root.url = github:srid/flake-root;
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+    rust-overlay.url = "github:oxalica/rust-overlay/master";
+    crane.url = "github:hoprnet/crane/este/find-filter";
+    pre-commit.url = "github:cachix/pre-commit-hooks.nix";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-root.url = "github:srid/flake-root";
 
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     pre-commit.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,13 +17,33 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-parts, flake-root, rust-overlay, crane, pre-commit, treefmt-nix, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      flake-parts,
+      flake-root,
+      rust-overlay,
+      crane,
+      pre-commit,
+      treefmt-nix,
+      ...
+    }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.treefmt-nix.flakeModule
         inputs.flake-root.flakeModule
       ];
-      perSystem = { config, lib, self', inputs', system, ... }:
+      perSystem =
+        {
+          config,
+          lib,
+          self',
+          inputs',
+          system,
+          ...
+        }:
         let
           rev = toString (self.shortRev or self.dirtyShortRev);
           fs = lib.fileset;
@@ -63,51 +83,79 @@
           };
 
           rust-builder-local = import ./nix/rust-builder.nix {
-            inherit nixpkgs rust-overlay crane localSystem;
+            inherit
+              nixpkgs
+              rust-overlay
+              crane
+              localSystem
+              ;
           };
 
           rust-builder-local-nightly = import ./nix/rust-builder.nix {
-            inherit nixpkgs rust-overlay crane localSystem;
+            inherit
+              nixpkgs
+              rust-overlay
+              crane
+              localSystem
+              ;
             useRustNightly = true;
           };
 
-          rust-builder-x86_64-linux = import ./nix/rust-builder.nix
-            { inherit nixpkgs rust-overlay crane localSystem; };
+          rust-builder-x86_64-linux = import ./nix/rust-builder.nix {
+            inherit
+              nixpkgs
+              rust-overlay
+              crane
+              localSystem
+              ;
+          };
 
-          rust-builder-x86_64-darwin = import
-            ./nix/rust-builder.nix
-            {
-              inherit nixpkgs rust-overlay crane localSystem;
-              crossSystem = pkgs.lib.systems.examples.x86_64-darwin;
-              isCross = true;
-            };
+          rust-builder-x86_64-darwin = import ./nix/rust-builder.nix {
+            inherit
+              nixpkgs
+              rust-overlay
+              crane
+              localSystem
+              ;
+            crossSystem = pkgs.lib.systems.examples.x86_64-darwin;
+            isCross = true;
+          };
 
-          rust-builder-aarch64-linux = import
-            ./nix/rust-builder.nix
-            {
-              inherit nixpkgs rust-overlay crane localSystem;
-              crossSystem = pkgs.lib.systems.examples.aarch64-multiplatform;
-              isCross = true;
-            };
+          rust-builder-aarch64-linux = import ./nix/rust-builder.nix {
+            inherit
+              nixpkgs
+              rust-overlay
+              crane
+              localSystem
+              ;
+            crossSystem = pkgs.lib.systems.examples.aarch64-multiplatform;
+            isCross = true;
+          };
 
-          rust-builder-aarch64-darwin = import
-            ./nix/rust-builder.nix
-            {
-              inherit nixpkgs rust-overlay crane localSystem;
-              crossSystem = pkgs.lib.systems.examples.aarch64-darwin // {
-                # comes with invalid config entry for this architecture: "arm64-apple-darwin" <- invalid rust compilation target
-                config = "aarch64-apple-darwin";
-              };
-              isCross = true;
+          rust-builder-aarch64-darwin = import ./nix/rust-builder.nix {
+            inherit
+              nixpkgs
+              rust-overlay
+              crane
+              localSystem
+              ;
+            crossSystem = pkgs.lib.systems.examples.aarch64-darwin // {
+              # comes with invalid config entry for this architecture: "arm64-apple-darwin" <- invalid rust compilation target
+              config = "aarch64-apple-darwin";
             };
+            isCross = true;
+          };
 
-          rust-builder-armv7l-linux = import
-            ./nix/rust-builder.nix
-            {
-              inherit nixpkgs rust-overlay crane localSystem;
-              crossSystem = pkgs.lib.systems.examples.armv7l-hf-multiplatform;
-              isCross = true;
-            };
+          rust-builder-armv7l-linux = import ./nix/rust-builder.nix {
+            inherit
+              nixpkgs
+              rust-overlay
+              crane
+              localSystem
+              ;
+            crossSystem = pkgs.lib.systems.examples.armv7l-hf-multiplatform;
+            isCross = true;
+          };
 
           gnosisvpnBuildArgs = {
             inherit src depsSrc rev;
@@ -121,57 +169,42 @@
             CARGO_PROFILE = "intelmac";
           };
 
-          gnosisvpn = rust-builder-local.callPackage
-            ./nix/rust-package.nix
-            gnosisvpnBuildArgs;
+          gnosisvpn = rust-builder-local.callPackage ./nix/rust-package.nix gnosisvpnBuildArgs;
 
-          gnosisvpn-x86_64-linux = rust-builder-x86_64-linux.callPackage
-            ./nix/rust-package.nix
-            gnosisvpnBuildArgs;
+          gnosisvpn-x86_64-linux = rust-builder-x86_64-linux.callPackage ./nix/rust-package.nix gnosisvpnBuildArgs;
 
-          gnosisvpn-aarch64-linux = rust-builder-aarch64-linux.callPackage
-            ./nix/rust-package.nix
-            gnosisvpnBuildArgs;
-          gnosisvpn-armv7l-linux = rust-builder-armv7l-linux.callPackage
-            ./nix/rust-package.nix
-            gnosisvpnBuildArgs;
+          gnosisvpn-aarch64-linux = rust-builder-aarch64-linux.callPackage ./nix/rust-package.nix gnosisvpnBuildArgs;
+          gnosisvpn-armv7l-linux = rust-builder-armv7l-linux.callPackage ./nix/rust-package.nix gnosisvpnBuildArgs;
           # CAVEAT: must be built from a darwin system
-          gnosisvpn-x86_64-darwin = rust-builder-x86_64-darwin.callPackage
-            ./nix/rust-package.nix
-            gnosisvpnIntelBuildArgs;
+          gnosisvpn-x86_64-darwin = rust-builder-x86_64-darwin.callPackage ./nix/rust-package.nix gnosisvpnIntelBuildArgs;
           # CAVEAT: must be built from a darwin system
-          gnosisvpn-aarch64-darwin = rust-builder-aarch64-darwin.callPackage
-            ./nix/rust-package.nix
-            gnosisvpnBuildArgs;
+          gnosisvpn-aarch64-darwin = rust-builder-aarch64-darwin.callPackage ./nix/rust-package.nix gnosisvpnBuildArgs;
 
-          gnosisvpn-clippy = rust-builder-local.callPackage
-            ./nix/rust-package.nix
-            (gnosisvpnBuildArgs // { runClippy = true; });
-          gnosisvpn-test = rust-builder-local.callPackage
-            ./nix/rust-package.nix
-            (gnosisvpnBuildArgs // { runTests = true; });
-          gnosisvpn-debug = rust-builder-local.callPackage
-            ./nix/rust-package.nix
-            (gnosisvpnBuildArgs // { CARGO_PROFILE = "dev"; });
+          gnosisvpn-clippy = rust-builder-local.callPackage ./nix/rust-package.nix (
+            gnosisvpnBuildArgs // { runClippy = true; }
+          );
+          gnosisvpn-test = rust-builder-local.callPackage ./nix/rust-package.nix (
+            gnosisvpnBuildArgs // { runTests = true; }
+          );
+          gnosisvpn-debug = rust-builder-local.callPackage ./nix/rust-package.nix (
+            gnosisvpnBuildArgs // { CARGO_PROFILE = "dev"; }
+          );
 
-          defaultDevShell = import
-            ./nix/shell.nix
-            { inherit pkgs config crane; };
+          defaultDevShell = import ./nix/shell.nix { inherit pkgs config crane; };
 
-          run-check = flake-utils.lib.mkApp
-            {
-              drv = pkgs.writeShellScriptBin "run-check" ''
-                set -e
-                check=$1
-                if [ -z "$check" ]; then
-                  nix flake show --json 2>/dev/null | \
-                    jq -r '.checks."${system}" | to_entries | .[].key' | \
-                    xargs -I '{}' nix build ".#checks."${system}".{}"
-                else
-                	nix build ".#checks."${system}".$check"
-                fi
-              '';
-            };
+          run-check = flake-utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "run-check" ''
+              set -e
+              check=$1
+              if [ -z "$check" ]; then
+                nix flake show --json 2>/dev/null | \
+                  jq -r '.checks."${system}" | to_entries | .[].key' | \
+                  xargs -I '{}' nix build ".#checks."${system}".{}"
+              else
+              	nix build ".#checks."${system}".$check"
+              fi
+            '';
+          };
         in
         {
           treefmt = {
@@ -185,8 +218,12 @@
             ];
 
             programs.shfmt.enable = true;
-            settings.formatter.shfmt.options = lib.mkForce [ "-s" "-w" "-i" "4" ];
-
+            settings.formatter.shfmt.options = lib.mkForce [
+              "-s"
+              "-w"
+              "-i"
+              "4"
+            ];
 
             programs.yamlfmt.enable = true;
             settings.formatter.yamlfmt.includes = [ ".github/workflows/*.yaml" ];
@@ -199,12 +236,18 @@
             };
 
             programs.prettier.enable = true;
-            settings.formatter.prettier.includes = [ "*.md" "*.json" ];
-            settings.formatter.prettier.excludes = [ "*.yml" "*.yaml" ];
+            settings.formatter.prettier.includes = [
+              "*.md"
+              "*.json"
+            ];
+            settings.formatter.prettier.excludes = [
+              "*.yml"
+              "*.yaml"
+            ];
 
             programs.rustfmt.enable = true;
 
-            programs.nixpkgs-fmt.enable = true;
+            programs.nixfmt.enable = true;
 
             programs.taplo.enable = true;
           };
@@ -225,7 +268,6 @@
             # Follow https://github.com/nixos/nixpkgs/pull/256590
             inherit gnosisvpn-aarch64-darwin gnosisvpn-x86_64-darwin;
 
-
             default = gnosisvpn;
           };
 
@@ -234,6 +276,11 @@
           formatter = config.treefmt.build.wrapper;
         };
       # platforms which are supported as build environments
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
     };
 }
--- nix/rust-package.nix
+++ nix/rust-package.nix
@@ -1,26 +1,27 @@
-{ buildDocs ? false
-, CARGO_PROFILE ? "release"
-, cargoExtraArgs ? ""
-, cargoToml
-, craneLib
-, curl
-, depsSrc
-, git
-, html-tidy
-, isCross
-, lib
-, libiconv
-, makeSetupHook
-, pandoc
-, pkg-config
-, pkgs
-, pkgsStatic
-, postInstall ? null
-, rev
-, runClippy ? false
-, runTests ? false
-, src
-, stdenv
+{
+  buildDocs ? false,
+  CARGO_PROFILE ? "release",
+  cargoExtraArgs ? "",
+  cargoToml,
+  craneLib,
+  depsSrc,
+  html-tidy,
+  isCross,
+  llvmPackages,
+  lib,
+  libiconv,
+  makeSetupHook,
+  mold,
+  pandoc,
+  pkg-config,
+  pkgs,
+  pkgsStatic,
+  postInstall ? null,
+  rev,
+  runClippy ? false,
+  runTests ? false,
+  src,
+  stdenv,
 }:
 let
   # `hostPlatform` is the cross-compilation output platform;
@@ -34,78 +35,93 @@ let
   pkgsStatOrDyn = if useMusl then pkgsStatic else pkgs;
 
   targetInterpreter =
-    if hostPlatform.isLinux && hostPlatform.isx86_64 then "/lib64/ld-linux-x86-64.so.2"
-    else if hostPlatform.isLinux && hostPlatform.isAarch64 then "/lib64/ld-linux-aarch64.so.1"
-    else if hostPlatform.isLinux && hostPlatform.isArmv7 then "/lib64/ld-linux-armhf.so.3"
-    else "";
+    if hostPlatform.isLinux && hostPlatform.isx86_64 then
+      "/lib64/ld-linux-x86-64.so.2"
+    else if hostPlatform.isLinux && hostPlatform.isAarch64 then
+      "/lib64/ld-linux-aarch64.so.1"
+    else if hostPlatform.isLinux && hostPlatform.isArmv7 then
+      "/lib64/ld-linux-armhf.so.3"
+    else
+      "";
 
   # The hook is used when building on darwin for non-darwin, where the flags
   # need to be cleaned up.
   darwinSuffixSalt = builtins.replaceStrings [ "-" "." ] [ "_" "_" ] buildPlatform.config;
   targetSuffixSalt = builtins.replaceStrings [ "-" "." ] [ "_" "_" ] hostPlatform.config;
-  setupHookDarwin = makeSetupHook
-    {
-      name = "darwin-hopr-gcc-hook";
-      substitutions = {
-        inherit darwinSuffixSalt targetSuffixSalt;
-      };
-    } ./setup-hook-darwin.sh;
+  setupHookDarwin = makeSetupHook {
+    name = "darwin-hopr-gcc-hook";
+    substitutions = { inherit darwinSuffixSalt targetSuffixSalt; };
+  } ./setup-hook-darwin.sh;
 
   crateInfo = craneLib.crateNameFromCargoToml { inherit cargoToml; };
   pname = crateInfo.pname;
-  pnameSuffix =
-    if CARGO_PROFILE == "release" then ""
-    else "-${CARGO_PROFILE}";
-  version = lib.strings.concatStringsSep "." (lib.lists.take 3 (builtins.splitVersion crateInfo.version));
+  pnameSuffix = if CARGO_PROFILE == "release" then "" else "-${CARGO_PROFILE}";
+  version = lib.strings.concatStringsSep "." (
+    lib.lists.take 3 (builtins.splitVersion crateInfo.version)
+  );
 
   isDarwinForDarwin = buildPlatform.isDarwin && hostPlatform.isDarwin;
   isDarwinForNonDarwin = buildPlatform.isDarwin && !hostPlatform.isDarwin;
 
   darwinBuildInputs =
     if isDarwinForDarwin || isDarwinForNonDarwin then
-      with pkgsStatOrDyn.pkgsBuildHost.darwin.apple_sdk.frameworks; [
+      with pkgsStatOrDyn.pkgsBuildHost.darwin.apple_sdk.frameworks;
+      [
         CoreFoundation
         CoreServices
         Security
         SystemConfiguration
-      ] else [ ];
+      ]
+    else
+      [ ];
   darwinNativeBuildInputs =
-    if !isDarwinForDarwin && isDarwinForNonDarwin then
-      [ setupHookDarwin ] else [ ];
+    if !isDarwinForDarwin && isDarwinForNonDarwin then [ setupHookDarwin ] else [ ];
 
   sharedArgsBase = {
-    inherit pname pnameSuffix version CARGO_PROFILE;
+    inherit
+      pname
+      pnameSuffix
+      version
+      CARGO_PROFILE
+      ;
 
     # FIXME: some dev dependencies depend on OpenSSL, would be nice to remove
     # this dependency
-    nativeBuildInputs = [ pkg-config libiconv ] ++ stdenv.extraNativeBuildInputs ++ darwinNativeBuildInputs;
+    nativeBuildInputs =
+      [
+        mold
+        llvmPackages.bintools
+        pkg-config
+        libiconv
+      ]
+      ++ stdenv.extraNativeBuildInputs
+      ++ darwinNativeBuildInputs;
     buildInputs = [ pkgsStatOrDyn.openssl ] ++ stdenv.extraBuildInputs ++ darwinBuildInputs;
 
     CARGO_HOME = ".cargo";
-    cargoExtraArgs = "--offline -p ${pname} ${cargoExtraArgs}";
-    # this env var is used by utoipa-swagger-ui to prevent internet access
-    CARGO_FEATURE_VENDORED = "true";
-    cargoVendorDir = "vendor";
+    cargoExtraArgs = "-p ${pname} ${cargoExtraArgs}";
     # disable running tests automatically for now
     doCheck = false;
-    # prevent nix from changing config.sub files under vendor
-    dontUpdateAutotoolsGnuConfigScripts = true;
     # set to the revision because during build the Git info is not available
     VERGEN_GIT_SHA = rev;
   };
 
   sharedArgs =
-    if runTests then sharedArgsBase // {
-      # exclude hopr-socks-server because it requires access to the internet
-      cargoTestExtraArgs = "--workspace";
-      doCheck = true;
-    }
-    else if runClippy then sharedArgsBase // { cargoClippyExtraArgs = "-- -Dwarnings"; }
-    else sharedArgsBase;
+    if runTests then
+      sharedArgsBase
+      // {
+        # exclude hopr-socks-server because it requires access to the internet
+        cargoTestExtraArgs = "--workspace";
+        doCheck = true;
+      }
+    else if runClippy then
+      sharedArgsBase // { cargoClippyExtraArgs = "-- -Dwarnings"; }
+    else
+      sharedArgsBase;
 
   docsArgs = {
     cargoArtifacts = null;
-    cargoExtraArgs = "--offline"; # overwrite the default to build all docs
+    cargoExtraArgs = ""; # overwrite the default to build all docs
     cargoDocExtraArgs = "--workspace --no-deps";
     RUSTDOCFLAGS = "--enable-index-page -Z unstable-options";
     CARGO_TARGET_DIR = "target/";
@@ -121,37 +137,37 @@ let
   };
 
   defaultArgs = {
-    cargoArtifacts = craneLib.buildDepsOnly (sharedArgs // {
-      src = depsSrc;
-      extraDummyScript = ''
-        cp -r --no-preserve=mode,ownership ${depsSrc}/vendor $out/
-        echo "# placeholder" > $out/vendor/config.toml
-      '';
-    });
+    cargoArtifacts = craneLib.buildDepsOnly (sharedArgs // { src = depsSrc; });
   };
 
   args = if buildDocs then sharedArgs // docsArgs else sharedArgs // defaultArgs;
 
   builder =
-    if runTests then craneLib.cargoTest
-    else if runClippy then craneLib.cargoClippy
-    else if buildDocs then craneLib.cargoDoc
-    else craneLib.buildPackage;
+    if runTests then
+      craneLib.cargoTest
+    else if runClippy then
+      craneLib.cargoClippy
+    else if buildDocs then
+      craneLib.cargoDoc
+    else
+      craneLib.buildPackage;
 in
-builder (args // {
-  inherit src postInstall;
+builder (
+  args
+  // {
+    inherit src postInstall;
 
-  preConfigure = ''
-    # respect the amount of available cores for building
-    export CARGO_BUILD_JOBS=$NIX_BUILD_CORES
-    echo "# placeholder" > vendor/config.toml
-  '';
+    preConfigure = ''
+      # respect the amount of available cores for building
+      export CARGO_BUILD_JOBS=$NIX_BUILD_CORES
+    '';
 
-  preFixup = lib.optionalString (isCross && targetInterpreter != "") ''
-    for f in `find $out/bin/ -type f`; do
-      echo "patching interpreter for $f to ${targetInterpreter}"
-      patchelf --set-interpreter ${targetInterpreter} --output $f.patched $f
-      mv $f.patched $f
-    done
-  '';
-})
+    preFixup = lib.optionalString (isCross && targetInterpreter != "") ''
+      for f in `find $out/bin/ -type f`; do
+        echo "patching interpreter for $f to ${targetInterpreter}"
+        patchelf --set-interpreter ${targetInterpreter} --output $f.patched $f
+        mv $f.patched $f
+      done
+    '';
+  }
+)
