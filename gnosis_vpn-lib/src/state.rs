a99df58df31c79c02d933abbb1ad19522bc464d4
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-07-15 06:43:08 +0200

    chore: update and consolidate dependency management (#124)
    * update backoff to latest greatest
    
    * update bincode to latest greatest
    
    * update bincode to latest greatest
    
    * update clap
    
    * updated crossbeam-channel
    
    * update directories
    
    * fix bincode usage with new version
    
    * update exitcode
    
    * consolidate humantime
    
    * consolidate humantime-serde
    
    * update notify to latest version
    
    * added crossbeam channel dep to notify
    
    * updated ping
    
    * updated rand
    
    * updated rand usage
    
    * fixed serde usgae
    
    * consolidated serde_json dep
    
    * consolidated thiserror
    
    * updated toml
    
    * consolidated tracing
    
    * consolidated url
    
    * further consolidated cargo toml
    
    * fix clippy errors

--- gnosis_vpn-lib/src/state.rs
+++ gnosis_vpn-lib/src/state.rs
@@ -1,12 +1,12 @@
-use serde::{Deserialize, Serialize};
+use bincode::{Decode, Encode};
 use std::default::Default;
-use std::fs;
+use std::fs::{self, File};
 use std::path::PathBuf;
 use thiserror::Error;
 
 use crate::dirs;
 
-#[derive(Default, Debug, Deserialize, Serialize)]
+#[derive(Default, Debug, Decode, Encode)]
 pub struct State {
     wg_private_key: Option<String>,
 }
@@ -21,32 +21,33 @@ pub enum Error {
     NoParentFolder,
     #[error("IO error: {0}")]
     IO(#[from] std::io::Error),
-    #[error("Serialization/Deserialization error: {0}")]
-    BinCodeError(#[from] bincode::Error),
+    #[error("Serialization error: {0}")]
+    BinCodeEncodeError(#[from] bincode::error::EncodeError),
+    #[error("Deserialization error: {0}")]
+    BinCodeDecodeError(#[from] bincode::error::DecodeError),
 }
 
 fn path() -> Option<PathBuf> {
     let project_dirs = dirs::project()?;
     let data_dir = project_dirs.data_local_dir();
-    let state_file = data_dir.join("state.bin");
+    let state_file = data_dir.join("state-v2.bin");
     Some(state_file)
 }
 
 pub fn read() -> Result<State, Error> {
     let p = path().ok_or(Error::NoStateFolder)?;
-    let content = fs::read(p).map_err(|e| {
+    let mut f = File::open(&p).map_err(|e| {
         if e.kind() == std::io::ErrorKind::NotFound {
             Error::NoFile
         } else {
             Error::IO(e)
         }
     })?;
-    let state: State = bincode::deserialize(&content[..]).map_err(Error::BinCodeError)?;
-    Ok(state)
+    bincode::decode_from_std_read(&mut f, bincode::config::standard()).map_err(Error::BinCodeDecodeError)
 }
 
 impl State {
-    pub fn set_wg_private_key(&mut self, key: String) -> Result<(), Error> {
+    pub fn set_wg_private_key(&mut self, key: String) -> Result<usize, Error> {
         self.wg_private_key = Some(key);
         self.write()
     }
@@ -55,11 +56,11 @@ impl State {
         self.wg_private_key.clone()
     }
 
-    fn write(&self) -> Result<(), Error> {
+    fn write(&self) -> Result<usize, Error> {
         let path = path().ok_or(Error::NoStateFolder)?;
-        let content = bincode::serialize(&self)?;
         let parent = path.parent().ok_or(Error::NoParentFolder)?;
         fs::create_dir_all(parent)?;
-        fs::write(path, content).map_err(Error::IO)
+        let mut f = File::create(&path)?;
+        bincode::encode_into_std_write(self, &mut f, bincode::config::standard()).map_err(Error::BinCodeEncodeError)
     }
 }
