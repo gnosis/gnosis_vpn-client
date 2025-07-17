dd0b91a1a5d31510e503e6b44974385cdb6f1225
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-07-17 21:54:25 +0200

    adjusted entry node

--- gnosis_vpn-lib/src/entry_node.rs
+++ gnosis_vpn-lib/src/entry_node.rs
@@ -1,6 +1,7 @@
 use thiserror::Error;
 use url::Url;
 
+use std::fmt::{self, Display};
 use std::time::Duration;
 
 use crate::session::Session;
@@ -11,6 +12,7 @@ pub struct EntryNode {
     pub api_token: String,
     pub listen_host: String,
     pub session_timeout: Duration,
+    pub api_version: APIVersion,
 }
 
 #[derive(Error, Debug)]
@@ -19,13 +21,25 @@ pub enum Error {
     NoHost,
 }
 
+#[derive(Clone, Debug)]
+pub enum APIVersion {
+    V4,
+}
+
 impl EntryNode {
-    pub fn new(endpoint: &Url, api_token: &str, listen_host: &str, session_timeout: &Duration) -> Self {
+    pub fn new(
+        endpoint: Url,
+        api_token: String,
+        listen_host: String,
+        session_timeout: Duration,
+        api_version: APIVersion,
+    ) -> Self {
         Self {
-            endpoint: endpoint.clone(),
-            api_token: api_token.to_string(),
-            listen_host: listen_host.to_string(),
-            session_timeout: *session_timeout,
+            endpoint,
+            api_token,
+            listen_host,
+            session_timeout,
+            api_version,
         }
     }
 
@@ -36,5 +50,19 @@ impl EntryNode {
     pub fn endpoint_with_port(&self, port: u16) -> Result<String, Error> {
         let host = self.endpoint.host_str().ok_or(Error::NoHost)?;
         Ok(format!("{host}:{port}"))
+    }
+}
+
+impl Display for APIVersion {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_ref())
+    }
+}
+
+impl AsRef<str> for APIVersion {
+    fn as_ref(&self) -> &str {
+        match self {
+            APIVersion::V4 => "v4",
+        }
     }
 }
