a2f3eb7d2e49f6e355c839179d5d0e4b626655a0
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-07-22 14:24:49 +0200

    use actual wg overhead

--- gnosis_vpn-lib/src/wg_tooling.rs
+++ gnosis_vpn-lib/src/wg_tooling.rs
@@ -221,8 +221,8 @@ AllowedIPs = {allowed_ips}
             endpoint = peer.endpoint,
             allowed_ips = allowed_ips,
             listen_port_line = listen_port_line,
-            // subtract 32 bytes WireGuard overhead from MTU
-            mtu = interface.mtu - 32,
+            // subtract 38 bytes WireGuard overhead from MTU
+            mtu = interface.mtu - 38,
         )
     }
 }
