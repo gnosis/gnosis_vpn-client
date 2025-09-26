ef99a3d9c7dd3baf8e1de4c17eb10800c0ccfdca
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-09-26 19:43:38 +0200

    prepping config

--- gnosis_vpn-lib/src/config/mod.rs
+++ gnosis_vpn-lib/src/config/mod.rs
@@ -1,21 +1,32 @@
+use edgli::hopr_lib::Address;
+use thiserror::Error;
+use url::Url;
+
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
-
-use edgli::hopr_lib::Address;
-use thiserror::Error;
 
 use crate::connection::{destination::Destination, options::Options};
 use crate::wg_tooling::Config as WireGuardConfig;
 
 mod v1;
+mod v2;
+mod v3;
+mod v4;
 
 pub const DEFAULT_PATH: &str = "/etc/gnosisvpn/config.toml";
 pub const ENV_VAR: &str = "GNOSISVPN_CONFIG_PATH";
 
+#[derive(Debug)]
+pub struct Hopr {
+    rpc_provider: Url,
+    identity_pass: String,
+    identity_file: Path,
+}
+
 #[derive(Clone, Debug)]
 pub enum Config {
-    V1(v1::Config),
+    V4(v4::Config),
 }
 
 #[derive(Debug, Error)]
@@ -30,6 +41,8 @@ pub enum Error {
     Deserialization(#[from] toml::de::Error),
     #[error("Unsupported config version: {0}")]
     VersionMismatch(u8),
+    #[error("Outdated config version: {0}")]
+    OutdatedVersion(u8),
 }
 
 pub fn read(path: &Path) -> Result<Config, Error> {
@@ -50,8 +63,23 @@ pub fn read(path: &Path) -> Result<Config, Error> {
     match version {
         1 => {
             tracing::warn!("found v1 configuration file, please update configuration file");
-            let res_v1 = toml::from_str::<v1::Config>(&content)?;
-            Ok(Config::V1(res_v1))
+            Err(Error::OutdatedVersion(1))
+        }
+        2 => {
+            tracing::warn!("found v2 configuration file, please update configuration file");
+            Err(Error::OutdatedVersion(2))
+        }
+        3 => {
+            tracing::warn!("found v3 configuration file, please update configuration file");
+            Err(Error::OutdatedVersion(3))
+        }
+        4 => {
+            let res = toml::from_str::<v4::Config>(&content)?;
+            let wrong_keys = v4::wrong_keys(&table);
+            for key in wrong_keys.iter() {
+                tracing::warn!(%key, "ignoring unsupported key in configuration file");
+            }
+            Ok(Config::V4(res))
         }
         _ => Err(Error::VersionMismatch(version as u8)),
     }
@@ -60,19 +88,25 @@ pub fn read(path: &Path) -> Result<Config, Error> {
 impl Config {
     pub fn destinations(&self) -> HashMap<Address, Destination> {
         match self {
-            Config::V1(config) => config.destinations(),
+            Config::V4(config) => config.destinations(),
         }
     }
 
     pub fn connection(&self) -> Options {
         match self {
-            Config::V1(config) => config.connection(),
+            Config::V4(config) => config.connection(),
         }
     }
 
     pub fn wireguard(&self) -> WireGuardConfig {
         match self {
-            Config::V1(config) => config.wireguard(),
+            Config::V4(config) => config.wireguard(),
+        }
+    }
+
+    pub fn hopr(&self) -> Hopr {
+        match self {
+            Config::V4(config) => config.hopr(),
         }
     }
 }
