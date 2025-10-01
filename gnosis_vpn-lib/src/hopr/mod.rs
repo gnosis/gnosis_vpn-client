a36784593b5f7aa9d52d794eb33560e0611c1776
Tibor Csóka <csokat@gmail.com>
2025-10-01 08:25:58 +0200

    chore(cli): remove the unnecessary env vars

--- gnosis_vpn-lib/src/hopr/mod.rs
+++ gnosis_vpn-lib/src/hopr/mod.rs
@@ -9,4 +9,3 @@ pub mod identity;
 pub const CONFIG_ENV: &str = "GNOSISVPN_HOPR_CONFIG_PATH";
 pub const ID_FILE_ENV: &str = "GNOSISVPN_HOPR_IDENTITY_FILE";
 pub const ID_PASS_ENV: &str = "GNOSISVPN_HOPR_IDENTITY_PASS";
-pub const DB_ENV: &str = "GNOSISVPN_HOPR_DB_PATH";
