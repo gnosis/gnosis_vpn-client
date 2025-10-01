77c85155d98c57ccfa9ea398fc24bb53eb231699
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-10-01 16:12:59 +0200

    changed to needed parameters

--- gnosis_vpn/src/cli.rs
+++ gnosis_vpn/src/cli.rs
@@ -1,6 +1,9 @@
 use clap::Parser;
-use gnosis_vpn_lib::{config, hopr, socket};
+use url::Url;
+
 use std::path::PathBuf;
+
+use gnosis_vpn_lib::{config, hopr, socket};
 
 /// Gnosis VPN system service - client application for Gnosis VPN connections
 #[derive(Debug, Parser)]
@@ -24,12 +27,17 @@ pub struct Cli {
         )]
     pub config_path: PathBuf,
 
+    /// RPC provider URL needed for fat Hopr edge client
+    #[arg(long, env = "RPC_PROVIDER")]
+    pub rpc_provider: Url,
+
     /// Hopr edge client configuration path
     #[arg(
         long,
         env = hopr::CONFIG_ENV,
+        default_value = None,
     )]
-    pub hopr_config_path: PathBuf,
+    pub hopr_config_path: Option<PathBuf>,
 
     /// Hopr edge client identity path
     #[arg(
