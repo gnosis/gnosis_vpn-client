a868714a6b613288d7b243439ead0e72fc21a40e
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2025-10-02 11:50:28 +0200

    feat: add provider instantiation

---
+++ gnosis_vpn-lib/src/chain/constants.rs
@@ -0,0 +1,8 @@
+use alloy::primitives::{Address, address};
+
+// wxHOPR Token contract address on Gnosis Chain
+pub const WXHOPR_TOKEN_ADDRESS: Address = address!("0xD4fdec44DB9D44B8f2b6d529620f9C0C7066A2c1");
+// Channels contract address in Rotsee network on Gnosis Chain. NOTE: This contract is network dependent.
+pub const CHANNELS_CONTRACT_ADDRESS: Address = address!("0x77C9414043d27fdC98A6A2d73fc77b9b383092a7");
+// NodeStakeFactory contract address in Rotsee network on Gnosis Chain. NOTE: This contract is network dependent.
+pub const NODE_STAKE_FACTORY_ADDRESS: Address = address!("0x439f5457FF58CEE941F7d946CB919c52EA30cfB3");
