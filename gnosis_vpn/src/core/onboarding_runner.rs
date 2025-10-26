80f47eeb269e7b9c733e49f1c07638c753278d83
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-10-26 10:28:57 +0100

    using message passing to get results

--- gnosis_vpn/src/core/onboarding_runner.rs
+++ gnosis_vpn/src/core/onboarding_runner.rs
@@ -1,10 +1,8 @@
-use edgli::hopr_lib::Address;
-use edgli::hopr_lib::exports::crypto::types::prelude::{ChainKeypair, Keypair};
-use tokio::sync::mpsc;
+use edgli::hopr_lib::exports::crypto::types::prelude::Keypair;
+use thiserror::Error;
 
 use gnosis_vpn_lib::balance::{self};
 
-use crate::core::runner_results::RunnerResults;
 use crate::hopr_params::{self, HoprParams};
 
 #[derive(Debug)]
@@ -12,11 +10,12 @@ pub struct OnboardingRunner {
     hopr_params: HoprParams,
 }
 
-#[derive(Debug)]
-pub enum Results {
-    HoprParamsError(hopr_params::Error),
-    PreSafeError(balance::Error),
-    Success(balance::PreSafe),
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    HoprParams(#[from] hopr_params::Error),
+    #[error(transparent)]
+    PreSafe(#[from] balance::Error),
 }
 
 impl OnboardingRunner {
@@ -24,25 +23,12 @@ impl OnboardingRunner {
         Self { hopr_params }
     }
 
-    pub async fn start(&self, sender: mpsc::Sender<RunnerResults>) {
-        let keys = match self.hopr_params.calc_keys() {
-            Ok(keys) => keys,
-            Err(e) => {
-                let _ = sender.send(Results::HoprParamsError(e).into()).await;
-                return;
-            }
-        };
+    pub async fn start(&self) -> Result<balance::PreSafe, Error> {
+        let keys = self.hopr_params.calc_keys()?;
         let private_key = keys.chain_key.clone();
         let rpc_provider = self.hopr_params.rpc_provider.clone();
         let node_address = keys.chain_key.public().to_address();
-        let res = balance::PreSafe::fetch(&private_key, rpc_provider.as_str(), node_address).await;
-        match res {
-            Ok(presafe) => {
-                let _ = sender.send(Results::Success(presafe).into()).await;
-            }
-            Err(e) => {
-                let _ = sender.send(Results::PreSafeError(e).into()).await;
-            }
-        }
+        let res = balance::PreSafe::fetch(&private_key, rpc_provider.as_str(), node_address).await?;
+        Ok(res)
     }
 }
