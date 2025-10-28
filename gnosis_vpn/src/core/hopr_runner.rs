9a66c6bee6e9ddfe12917c099832504bf7a1a2c2
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-10-28 14:32:37 +0100

    compiling with hopr runner

--- gnosis_vpn/src/core/hopr_runner.rs
+++ gnosis_vpn/src/core/hopr_runner.rs
@@ -1,20 +1,21 @@
-use thiserror::Error;
 use edgli::hopr_lib::state::HoprState;
-use tokio::sync::mpsc;
+use edgli::hopr_lib::{Balance, WxHOPR};
+use thiserror::Error;
+use tokio::sync::{mpsc, oneshot};
 
-use gnosis_vpn_lib::chain::contracts::NetworkSpecifications;
-use gnosis_vpn_lib::ticket_stats::{self, TicketStats};
+use gnosis_vpn_lib::hopr::{Hopr, HoprError, config as hopr_config};
 
 use crate::hopr_params::{self, HoprParams};
 
 #[derive(Debug)]
 pub struct HoprRunner {
     hopr_params: HoprParams,
+    ticket_value: Balance<WxHOPR>,
 }
 
 #[derive(Debug)]
-pub struct Cmd {
-    Shutdown{ rsp: oneshot::Sender<()> },
+pub enum Cmd {
+    Shutdown { rsp: oneshot::Sender<()> },
     Status { rsp: oneshot::Sender<HoprState> },
 }
 
@@ -22,11 +23,18 @@ pub struct Cmd {
 pub enum Error {
     #[error(transparent)]
     HoprParams(#[from] hopr_params::Error),
+    #[error(transparent)]
+    HoprConfig(#[from] hopr_config::Error),
+    #[error(transparent)]
+    Hopr(#[from] HoprError),
 }
 
 impl HoprRunner {
-    pub fn new(hopr: Hopr) -> Self {
-        Self { hopr }
+    pub fn new(hopr_params: HoprParams, ticket_value: Balance<WxHOPR>) -> Self {
+        Self {
+            hopr_params,
+            ticket_value,
+        }
     }
 
     pub async fn start(&self, cmd_receiver: &mut mpsc::Receiver<Cmd>) -> Result<(), Error> {
@@ -37,25 +45,23 @@ impl HoprRunner {
             hopr_params::ConfigFileMode::Generated => hopr_config::generate(
                 self.hopr_params.network.clone(),
                 self.hopr_params.rpc_provider.clone(),
-                ticket_value,
+                self.ticket_value,
             )?,
         };
         let keys = self.hopr_params.calc_keys()?;
-        let hoprd = Hopr::new(cfg, keys).await?;
-        while Some(cmd) = cmd_receiver.recv().await {
+        let mut hoprd = Hopr::new(cfg, keys).await?;
+        while let Some(cmd) = cmd_receiver.recv().await {
             match cmd {
                 Cmd::Shutdown { rsp } => {
-                    hoprd.shutdown().await?;
-                    let _ = rsp.send(())?;
+                    hoprd.shutdown().await;
+                    rsp.send(());
                     break;
                 }
                 Cmd::Status { rsp } => {
-                    let state = hoprd.get_state().await?;
-                    let _ = rsp.send(state)?;
+                    rsp.send(hoprd.status());
                 }
             }
-
         }
+        Ok(())
     }
-
 }
