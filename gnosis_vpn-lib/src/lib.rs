325de5430574f96862f52fb56c38e7dcaec9ac1d
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-10-29 08:14:49 +0100

    fixed funding tool runner

--- gnosis_vpn-lib/src/lib.rs
+++ gnosis_vpn-lib/src/lib.rs
@@ -11,6 +11,7 @@ pub mod metrics;
 pub mod network;
 pub mod node;
 pub mod onboarding;
+pub mod remote_data;
 pub mod session;
 pub mod socket;
 pub mod ticket_stats;
@@ -19,7 +20,6 @@ pub mod wg_tooling;
 mod dirs;
 mod gvpn_client;
 mod ping;
-mod remote_data;
 
 pub mod prelude {
     pub use edgli::hopr_lib::{Address, Hopr, HoprKeys, config::HoprLibConfig};
