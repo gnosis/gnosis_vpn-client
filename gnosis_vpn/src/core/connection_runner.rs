767d57f2877fbf3bba76896dff0961b3198456b4
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-11-03 07:08:01 +0100

    connecting correctly

--- gnosis_vpn/src/core/connection_runner.rs
+++ gnosis_vpn/src/core/connection_runner.rs
@@ -2,18 +2,17 @@ use backoff::ExponentialBackoff;
 use backoff::future::retry;
 use edgli::hopr_lib::SessionClientConfig;
 use thiserror::Error;
-use tokio::sync::{broadcast, mpsc};
+use tokio::sync::mpsc;
 use uuid::{self, Uuid};
 
 use gnosis_vpn_lib::connection::destination::Destination;
 use gnosis_vpn_lib::connection::options::Options;
 use gnosis_vpn_lib::gvpn_client::{self, Registration};
-use gnosis_vpn_lib::hopr::Hopr;
 use gnosis_vpn_lib::hopr::types::SessionClientMetadata;
+use gnosis_vpn_lib::hopr::{Hopr, HoprError};
 use gnosis_vpn_lib::wg_tooling;
 
 use crate::core::conn;
-use crate::core::hopr_runner::{Cmd, Evt as HoprEvt};
 
 use crate::hopr_params::{self, HoprParams};
 use std::sync::Arc;
@@ -28,11 +27,22 @@ pub struct ConnectionRunner {
 
 #[derive(Debug)]
 pub enum Evt {
-    OpeningSession(Uuid),
+    OpenBridge(Uuid),
+    Register(Uuid),
+    CloseBridge(Uuid),
+    OpenPing(Uuid),
+    Ping(Uuid),
+    AdjustMain(Uuid),
+    TunnelWg(Uuid),
 }
 
 #[derive(Debug, Error)]
-pub enum Error {}
+pub enum Error {
+    #[error(transparent)]
+    Hopr(#[from] HoprError),
+    #[error(transparent)]
+    GvpnClient(#[from] gvpn_client::Error),
+}
 
 impl ConnectionRunner {
     pub fn new(destination: Destination, options: Options, wg: wg_tooling::WireGuard, hoprd: Arc<Hopr>) -> Self {
@@ -45,14 +55,16 @@ impl ConnectionRunner {
         }
     }
 
-    pub async fn start(&self, evt_sender: mpsc::Sender<Evt>) -> Result<(), Error> {
-        evt_sender.send(Evt::OpeningSession(self.id)).await;
-        let res = self.open_bridge_session().await;
-        tracing::debug!("ConnectionRunner started");
-        unimplemented!()
+    pub async fn connect(&self, evt_sender: mpsc::Sender<Evt>) -> Result<(), Error> {
+        evt_sender.send(Evt::OpenBridge(self.id)).await;
+        let session_client_metadata = self.open_bridge_session().await?;
+        evt_sender.send(Evt::Register(self.id)).await;
+        let registration = self.register(&session_client_metadata).await?;
+        evt_sender.send(Evt::CloseBridge(self.id)).await;
+        Ok(())
     }
 
-    async fn open_bridge_session(&self) {
+    async fn open_bridge_session(&self) -> Result<SessionClientMetadata, HoprError> {
         let cfg = SessionClientConfig {
             capabilities: self.options.sessions.bridge.capabilities,
             forward_path_options: self.destination.routing.clone(),
@@ -63,16 +75,36 @@ impl ConnectionRunner {
             )),
             ..Default::default()
         };
-        let res = self
-            .hoprd
-            .open_session(
-                self.destination.address,
-                self.options.sessions.bridge.target.clone(),
-                Some(1),
-                Some(1),
-                cfg,
-            )
-            .await;
-        tracing::info!(res = ?res, "Opened bridge session");
+        retry(ExponentialBackoff::default(), || async {
+            let res = self
+                .hoprd
+                .open_session(
+                    self.destination.address,
+                    self.options.sessions.bridge.target.clone(),
+                    Some(1),
+                    Some(1),
+                    cfg.clone(),
+                )
+                .await?;
+            Ok(res)
+        })
+        .await
+    }
+
+    async fn register(
+        &self,
+        session_client_metadata: &SessionClientMetadata,
+    ) -> Result<Registration, gvpn_client::Error> {
+        let input = gvpn_client::Input::new(
+            self.wg.key_pair.public_key.clone(),
+            session_client_metadata.bound_host.port(),
+            self.options.timeouts.http,
+        );
+        let client = reqwest::Client::new();
+        retry(ExponentialBackoff::default(), || async {
+            let res = gvpn_client::register(&client, &input).await?;
+            Ok(res)
+        })
+        .await
     }
 }
