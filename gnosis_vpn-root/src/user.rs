428fe0eea59f5ee1e2a83474d595956ccbea619e
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-11-26 22:44:09 +0100

    got process set up

--- gnosis_vpn-root/src/user.rs
+++ gnosis_vpn-root/src/user.rs
@@ -2,25 +2,24 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("User {NAME} not found")]
-    UserNotFound,
+    #[error("User not found: {0}")]
+    UserNotFound(String),
 }
 
+#[derive(Debug)]
 pub struct Worker {
     pub uid: u32,
     pub gid: u32,
 }
 
-const NAME: &str = "gnosisvpn";
-
 impl Worker {
-    pub fn from_system() -> Result<Self, Error> {
-        users::get_user_by_name(NAME)
+    pub fn from_system(name: &str) -> Result<Self, Error> {
+        users::get_user_by_name(name)
             .map(|user| {
                 let uid = user.uid();
                 let gid = user.primary_group_id();
                 Worker { uid, gid }
             })
-            .ok_or(Error::UserNotFound)
+            .ok_or(Error::UserNotFound(name.to_string()))
     }
 }
--- gnosis_vpn-worker/src/cli.rs
+++ gnosis_vpn-worker/src/cli.rs
@@ -10,73 +10,10 @@ use gnosis_vpn_lib::{config, hopr, socket};
 /// Gnosis VPN system service - client application for Gnosis VPN connections
 #[derive(Clone, Debug, Parser)]
 #[command(version)]
-pub struct Cli {
-    /// Socket path for communication with this servive
-    #[arg(
-        short,
-        long,
-        env = socket::ENV_VAR,
-        default_value = socket::DEFAULT_PATH
-    )]
-    pub socket_path: PathBuf,
-
-    /// General configuration file
-    #[arg(
-        short,
-        long,
-        env = config::ENV_VAR,
-        default_value = config::DEFAULT_PATH,
-        )]
-    pub config_path: PathBuf,
-
-    /// RPC provider URL needed for fat Hopr edge client
-    #[arg(long, env = hopr::RPC_PROVIDER_ENV)]
-    pub hopr_rpc_provider: Url,
-
-    /// Hopr network
-    #[arg(long, env = hopr::NETWORK_ENV, default_value = "dufour")]
-    pub hopr_network: Network,
-
-    /// Hopr edge client configuration path
-    #[arg( long, env = hopr::CONFIG_ENV, default_value = None) ]
-    pub hopr_config_path: Option<PathBuf>,
-
-    /// Hopr edge client identity path
-    #[arg( long, env = hopr::ID_FILE_ENV, default_value = None)]
-    pub hopr_identity_file: Option<PathBuf>,
-
-    /// Hopr edge client identity pass
-    #[arg( long, env = hopr::ID_PASS_ENV, default_value = None)]
-    pub hopr_identity_pass: Option<String>,
-
-    /// Allow insecure non-private connections (only for testing purposes)
-    #[arg(long)]
-    pub allow_insecure: bool,
-}
+pub struct Cli {}
 
 pub fn parse() -> Cli {
     Cli::parse()
-}
-
-impl From<Cli> for HoprParams {
-    fn from(cli: Cli) -> Self {
-        let network = cli.hopr_network;
-        let rpc_provider = cli.hopr_rpc_provider;
-        let config_mode = match cli.hopr_config_path {
-            Some(path) => hopr_params::ConfigFileMode::Manual(path),
-            None => hopr_params::ConfigFileMode::Generated,
-        };
-        let allow_insecure = cli.allow_insecure;
-
-        HoprParams::new(
-            cli.hopr_identity_file,
-            cli.hopr_identity_pass,
-            config_mode,
-            network,
-            rpc_provider,
-            allow_insecure,
-        )
-    }
 }
 
 #[cfg(test)]
