2d9a88cab23f7095b465c1b78da5f28dca346716
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-12-01 21:23:28 +0100

    prepping for macos routing

--- gnosis_vpn-lib/src/routing/linux.rs
+++ gnosis_vpn-lib/src/routing/linux.rs
@@ -5,49 +5,38 @@ use super::Error;
 #[derive(Debug)]
 pub struct Routing {
     worker: user::Worker,
-    strategy: Strategy,
 }
-
-#[derive(Debug)]
-enum Strategy {
-    CgroupBypass,
-    IpRule,
-}
-
-const CGROUP: &str = "hoprnet";
 
 /**
  * Refactor logic to use:
  * - [rtnetlink](https://docs.rs/rtnetlink/latest/rtnetlink/index.html)
- * - [cgroup_rs](https://docs.rs/cgroups-rs/0.5.0/cgroups_rs/index.html)
  */
 impl Routing {
     pub fn new(worker: user::Worker) -> Result<Self, Error> {
-        let strategy = if supports_cgroup_bypass()? {
-            Strategy::CgroupBypass
-        } else {
-            Strategy::IpRule
-        };
-
-        Ok(Routing { worker, strategy })
-    }
-
-    fn supports_cgroup_bypass() -> Result<bool, Error> {
-        let output = Command::new("mount").output().await?;
-
-        if output.status.success() {
-Ok(output.contains("cgroup2 "))
-        } else {
-           let stdout =  String::from_utf8_lossy(&output.stdout);
-           let stderr =  String::from_utf8_lossy(&output.stderr);
-               tracing::error!(%stdout, %stderr, status = %output.status, "cgroup check mount command failed");
-               Err(Error::MountCmdFailed)
-        }
+        Ok(Routing { worker })
     }
 
     pub fn setup(&mut self) -> Result<(), Error> {
-        let output = Command::new("mkdir").arg("-p").arg(format!("/sys/fs/cgroup/net_cls/{CGROUP}")).
-            status().await?
-echo 0xCAFE > /sys/fs/cgroup/net_cls/bypass_vpn/net_cls.classid
+        let output = Command::new("ip")
+            .arg("rule")
+            .arg("add")
+            .arg("uidrange")
+            .arg(format!("{}-{}", self.worker.uid, self.worker.uid))
+            .arg("lookup")
+            .arg("main")
+            .arg("priority")
+            .arg("100")
+            .output()
+            .await?;
+
+        if output.status.success() {
+            Ok(())
+        } else {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            let status_code = output.status.code();
+            tracing::error!(%status_code, %stdout, %stderr, "Failed to add ip rule");
+            Err(Error::IpRuleSetup(status_code))
+        }
     }
 }
