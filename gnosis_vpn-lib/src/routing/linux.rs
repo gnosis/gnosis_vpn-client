02211b919e78cdf2d14313e3dc402a7543a212f7
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-12-02 21:49:51 +0100

    got setup compiling

--- gnosis_vpn-lib/src/routing/linux.rs
+++ gnosis_vpn-lib/src/routing/linux.rs
@@ -5,32 +5,21 @@ use crate::worker;
 
 use super::Error;
 
-#[derive(Debug)]
-pub struct Routing {
-    worker: worker::Worker,
-}
-
 /**
  * Refactor logic to use:
  * - [rtnetlink](https://docs.rs/rtnetlink/latest/rtnetlink/index.html)
  */
-impl Routing {
-    pub fn new(worker: worker::Worker) -> Result<Self, Error> {
-        Ok(Routing { worker })
-    }
-
-    pub async fn setup(&mut self) -> Result<(), Error> {
-        Command::new("ip")
-            .arg("rule")
-            .arg("add")
-            .arg("uidrange")
-            .arg(format!("{}-{}", self.worker.uid, self.worker.uid))
-            .arg("lookup")
-            .arg("main")
-            .arg("priority")
-            .arg("100")
-            .run()
-            .await
-            .map_err(Error::from)
-    }
+pub async fn setup(worker: &worker::Worker) -> Result<(), Error> {
+    Command::new("ip")
+        .arg("rule")
+        .arg("add")
+        .arg("uidrange")
+        .arg(format!("{}-{}", worker.uid, worker.uid))
+        .arg("lookup")
+        .arg("main")
+        .arg("priority")
+        .arg("100")
+        .run()
+        .await
+        .map_err(Error::from)
 }
