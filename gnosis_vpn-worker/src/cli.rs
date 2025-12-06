4ae6675d7ca21c42ea861f5a2084909baad98cf9
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-12-06 20:56:13 +0100

    removed unused cli parsing on worker

--- gnosis_vpn-worker/src/cli.rs
+++ gnosis_vpn-worker/src/cli.rs
@@ -1,15 +1,9 @@
 use clap::Parser;
 
-
-
 /// Gnosis VPN system service - client application for Gnosis VPN connections
 #[derive(Clone, Debug, Parser)]
 #[command(version)]
 pub struct Cli {}
-
-pub fn parse() -> Cli {
-    Cli::parse()
-}
 
 #[cfg(test)]
 mod tests {
