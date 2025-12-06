e57d47a137bd1f78959528f476fb5e19d73b96db
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-12-06 09:35:49 +0100

    better events

--- gnosis_vpn-lib/src/event/mod.rs
+++ gnosis_vpn-lib/src/event/mod.rs
@@ -1,5 +1,37 @@
-mod incoming;
-mod outgoing;
+//! This module holds event definitions for inter process communication between root <-> worker and worker <-> core.
 
-pub use incoming::Incoming;
-pub use outgoing::Outgoing;
+use serde::{Deserialize, Serialize};
+use tokio::sync::oneshot;
+
+use crate::command::{Command, Response};
+use crate::config::Config;
+use crate::hopr_params::HoprParams;
+
+#[derive(Debug)]
+pub enum IncomingCore {
+    Command {
+        cmd: Command,
+        resp: oneshot::Sender<Response>,
+    },
+    Shutdown,
+}
+
+#[derive(Debug)]
+pub enum OutgoingCore {
+    WgUp(String),
+    WgDown,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum IncomingWorker {
+    HoprParams { hopr_params: HoprParams },
+    Config { config: Config },
+    Shutdown,
+    Command { cmd: Command },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum OutoingWorker {
+    Response { resp: Box<Response> },
+    Ack,
+}
--- gnosis_vpn-lib/src/event/outgoing.rs
+++
@@ -1,7 +0,0 @@
-//! This module indicates internal events that come from the core application loop.
-
-#[derive(Debug, Clone)]
-pub enum Outgoing {
-    WgUp(String),
-    WgDown,
-}
