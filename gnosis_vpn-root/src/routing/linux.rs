d7b72adaf53256633ad4101cb67b1d2b2ce53ee1
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-12-07 21:11:58 +0100

    exempt ping from bypassing

--- gnosis_vpn-root/src/routing/linux.rs
+++ gnosis_vpn-root/src/routing/linux.rs
@@ -1,7 +1,7 @@
 use tokio::process::Command;
 
 use gnosis_vpn_lib::shell_command_ext::ShellCommandExt;
-use gnosis_vpn_lib::worker;
+use gnosis_vpn_lib::{wireguard, worker};
 
 use super::Error;
 
@@ -28,9 +28,9 @@ pub async fn setup(worker: &worker::Worker) -> Result<(), Error> {
         .arg("MARK")
         .arg("--set-mark")
         .arg(MARK)
-        .spawn_no_capture()
-        .await
-        .map_err(Error::from)?;
+        .run()
+        .await?;
+
     // save mark of those outgoing packages
     // iptables -t mangle -A OUTPUT -m mark --mark 0xDEAD -j CONNMARK --save-mark;
     Command::new("iptables")
@@ -45,9 +45,8 @@ pub async fn setup(worker: &worker::Worker) -> Result<(), Error> {
         .arg("-j")
         .arg("CONNMARK")
         .arg("--save-mark")
-        .spawn_no_capture()
-        .await
-        .map_err(Error::from)?;
+        .run()
+        .await?;
     // restore mark on incoming packages belonging to those connections, so they can bypass routing
     // iptables -t mangle -I PREROUTING 1 -j CONNMARK --restore-mark
     Command::new("iptables")
@@ -59,9 +58,8 @@ pub async fn setup(worker: &worker::Worker) -> Result<(), Error> {
         .arg("-j")
         .arg("CONNMARK")
         .arg("--restore-mark")
-        .spawn_no_capture()
-        .await
-        .map_err(Error::from)?;
+        .run()
+        .await?;
     Ok(())
 }
 
@@ -80,9 +78,8 @@ pub async fn teardown(worker: &worker::Worker) -> Result<(), Error> {
         .arg("MARK")
         .arg("--set-mark")
         .arg(MARK)
-        .spawn_no_capture()
-        .await
-        .map_err(Error::from);
+        .run()
+        .await;
     let res2 = Command::new("iptables")
         .arg("-t")
         .arg("mangle")
@@ -95,9 +92,8 @@ pub async fn teardown(worker: &worker::Worker) -> Result<(), Error> {
         .arg("-j")
         .arg("CONNMARK")
         .arg("--save-mark")
-        .spawn_no_capture()
-        .await
-        .map_err(Error::from);
+        .run()
+        .await;
     let res3 = Command::new("iptables")
         .arg("-t")
         .arg("mangle")
@@ -107,15 +103,38 @@ pub async fn teardown(worker: &worker::Worker) -> Result<(), Error> {
         .arg("-j")
         .arg("CONNMARK")
         .arg("--restore-mark")
-        .spawn_no_capture()
-        .await
-        .map_err(Error::from);
-
+        .run()
+        .await;
     res1.and(res2).and(res3)?;
     Ok(())
 }
 
 pub async fn add_ip_rules(worker: &worker::Worker) -> Result<(), Error> {
+    // except wireguard subnet from bypassing traffic
+    // wg show wg0_gnosisvpn fwmark
+    let interface_parts: Vec<&str> = wireguard::WG_CONFIG_FILE.split('.').collect();
+    let interface = interface_parts[0];
+    let fwmark = Command::new("wg")
+        .arg("show")
+        .arg(interface)
+        .arg("fwmark")
+        .run_stdout()
+        .await?;
+    // TODO use dynamic ip from interface
+    // TODO setup for macos
+    // ip rule add to 10.128.0.0/24 lookup 0xca6c priority 50
+    Command::new("ip")
+        .arg("rule")
+        .arg("add")
+        .arg("to")
+        .arg("10.128.0.0/24")
+        .arg("lookup")
+        .arg(fwmark)
+        .arg("priority")
+        .arg("50")
+        .run()
+        .await?;
+
     // forward rules need to be applied after wg-quick up so that their priority is higher
     // wg-quick up is quite clever and adjusts it's own rule setting if our bypass rules are
     // applied too early
