45086940aa06a35d5935237b866ac55d173c4336
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-12-20 11:18:48 +0100

    updated processes interface

--- gnosis_vpn-lib/src/event/mod.rs
+++ gnosis_vpn-lib/src/event/mod.rs
@@ -3,73 +3,116 @@
 use serde::{Deserialize, Serialize};
 use tokio::sync::oneshot;
 
+use std::time::Duration;
+
 use crate::command::{Command, Response};
 use crate::config::Config;
 use crate::hopr_params::HoprParams;
+use crate::ping;
 use crate::wireguard::{self, WireGuard};
 
 /// Messages sent from worker to core application logic
 #[derive(Debug)]
-pub enum IncomingCore {
+pub enum WorkerToCore {
     /// Socket command avaiting response
     Command {
         cmd: Command,
         resp: oneshot::Sender<Response>,
     },
     Shutdown,
-    /// Result of WireGuard tooling execution
-    WgUpResult {
-        res: Result<(), String>,
-    },
 }
 
 /// Messages sent from core application logic to worker
-pub type OutgoingCore = WireGuardCommand;
+#[derive(Debug)]
+pub enum CoreToWorker {
+    /// Requesting root execution and waiting for response
+    RespondableRequestToRoot(RespondableRequestToRoot),
+}
 
 /// Messages sent from root to worker
 #[derive(Debug, Serialize, Deserialize)]
-pub enum IncomingWorker {
+pub enum RootToWorker {
     /// Startup parameters for hoprd
     HoprParams { hopr_params: HoprParams },
     /// Configuration file
     Config { config: Config },
     /// Socket command received by root
-    Command { cmd: Command },
-    /// Result of WireGuard tooling execution
-    WgUpResult { res: Result<(), String> },
+    Command(Command),
+    /// Result of a request to root
+    ResponseFromRoot(ResponseFromRoot),
 }
 
 /// Messages sent from worker to root
 #[derive(Debug, Serialize, Deserialize)]
-pub enum OutgoingWorker {
+pub enum WorkerToRoot {
     /// Response to a socket command
-    Response { resp: Box<Response> },
+    Response(Response),
     /// Acknowledgement other incoming messages
     Ack,
+    /// Request to root execution
+    RequestToRoot(RequestToRoot),
     /// Received unexpected event from root
     OutOfSync,
-    /// Instruct root to execute WireGuard tooling
-    WireGuard(WireGuardCommand),
 }
 
-/// WireGuard tooling execution commands
-#[derive(Debug, Serialize, Deserialize)]
-pub enum WireGuardCommand {
-    /// generated WireGuard configuration file
-    WgUp(WgData),
-    /// Tear down WireGuard interface
-    WgDown,
+/// Runner requesting root command and waiting for response
+#[derive(Debug)]
+pub enum RespondableRequestToRoot {
+    DynamicWgRouting {
+        wg_data: WireGuardData,
+        resp: oneshot::Sender<Result<(), String>>,
+    },
+    StaticWgRouting {
+        wg_data: WireGuardData,
+        peer_ips: Vec<String>,
+        resp: oneshot::Sender<Result<(), String>>,
+    },
+    TearDownWg {
+        resp: oneshot::Sender<Result<(), String>>,
+    },
+    Ping {
+        options: ping::Options,
+        resp: oneshot::Sender<Result<Duration, String>>,
+    },
 }
 
+/// Data required for WireGuard operations
 #[derive(Debug, Serialize, Deserialize)]
-pub struct WgData {
+pub struct WireGuardData {
     pub wg: WireGuard,
     pub interface_info: wireguard::InterfaceInfo,
     pub peer_info: wireguard::PeerInfo,
 }
 
-impl AsRef<IncomingWorker> for IncomingWorker {
-    fn as_ref(&self) -> &IncomingWorker {
+impl AsRef<RootToWorker> for RootToWorker {
+    fn as_ref(&self) -> &RootToWorker {
         self
     }
+}
+
+/// Root execution request without unserializable response channel from runner.
+/// Slimmed down **RespondableRequestToRoot** for inter-process communication.
+#[derive(Debug, Serialize, Deserialize)]
+pub enum RequestToRoot {
+    DynamicWgRouting {
+        wg_data: WireGuardData,
+    },
+    StaticWgRouting {
+        wg_data: WireGuardData,
+        peer_ips: Vec<String>,
+    },
+    TearDownWg,
+    Ping {
+        options: ping::Options,
+    },
+}
+
+/// Root execution response from root process.
+/// Should be matched by worker to **ResponsibleRequestToRoot** request responses.
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ResponseFromRoot {
+    DynamicWgRouting { res: Result<(), String> },
+    StaticWgRouting { res: Result<(), String> },
+    TearDownWg { res: Result<(), String> },
+    Ping { res: Result<Duration, String> },
 }
