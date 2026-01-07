a8d472eed175b602d70dd5fbb24f11858b406a20
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-01-07 16:25:50 +0100

    preparing ping fallback

--- gnosis_vpn-lib/src/ping.rs
+++ gnosis_vpn-lib/src/ping.rs
@@ -1,8 +1,11 @@
 use serde::{Deserialize, Serialize};
+use tokio::process::Command;
 use thiserror::Error;
 
 use std::net::{IpAddr, Ipv4Addr};
 use std::time::Duration;
+
+use crate::shell_command_ext::{ShellCommandExt};
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Options {
@@ -16,6 +19,8 @@ pub struct Options {
 pub enum Error {
     #[error("Ping failed")]
     PingFailed(#[from] ping::Error),
+    #[error("Ping timed out")]
+    Timeout,
 }
 
 impl Default for Options {
@@ -29,9 +34,39 @@ impl Default for Options {
     }
 }
 
-#[allow(dead_code)]
 #[tracing::instrument(name = "ping", ret)]
-pub fn ping(opts: &Options) -> Result<Duration, Error> {
+pub async fn ping(opts: &Options) -> Result<Duration, Error> {
+    // prefer system ping as it seems way more robust that ping crate
+    let available = Command::new("which")
+        .arg("ping")
+        .run().await;
+
+    match available {
+        Ok(_) => {
+            ping_using_cmd(opts)
+        }
+        Err(error) => {
+            tracing::warn!(?error, "Unable to use system ping cmd - fallback to internal ping");
+            ping_using_ping_crate(opts)
+        }
+        }
+
+}
+
+async fn ping_using_cmd(opts: &Options) -> Result<Duration, Error> {
+            let mut cmd = Command::new("ping").arg("-c").arg("1")
+    #[cfg(target_os = "linux")]
+    {
+        cmd = cmd.arg("-W").arg(opts.timeout)
+    }
+    #[cfg(target_os = "macos")]
+    {
+        cmd = cmd.arg("-t").arg(opts.timeout)
+    }
+    cmd.run().await.map_err(|_| Error::Timeout)
+}
+
+fn ping_using_ping_crate(opts: &Options) -> Result<Duration, Error> {
     let mut builder = ping::new(opts.address);
     let mut ping = builder.timeout(opts.timeout).ttl(opts.ttl).seq_cnt(opts.seq_count);
     #[cfg(target_os = "linux")]
