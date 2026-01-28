40c9594c2b94509244a7e8c039c3f9715ecd6e6b
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-01-28 14:24:27 +0100

    fix address on ping cmd

--- gnosis_vpn-lib/src/ping.rs
+++ gnosis_vpn-lib/src/ping.rs
@@ -65,6 +65,7 @@ async fn ping_using_cmd(opts: &Options) -> Result<Duration, Error> {
         cmd.arg("-t").arg(opts.timeout.as_secs().to_string());
         cmd.arg("-m").arg(opts.ttl.to_string());
     }
+    cmd.arg(opts.address.to_string());
     let output = cmd.run_stdout(Logs::Print).await.map_err(|_| Error::Timeout)?;
     parse_duration(output)
 }
