fbc8ae19b37639cdf585e4f050c3fc7dd5e3cebd
Tino Breddin <tino@hoprnet.org>
2026-02-03 14:53:42 +0100

    feat(build): Re-enable mimalloc on Linux

--- gnosis_vpn-worker/src/main.rs
+++ gnosis_vpn-worker/src/main.rs
@@ -18,10 +18,9 @@ mod cli;
 mod init;
 // Avoid musl's default allocator due to degraded performance
 // https://nickb.dev/blog/default-musl-allocator-considered-harmful-to-performance
-// HOWEVER disabled due to build issues
-// #[cfg(target_os = "linux")]
-// #[global_allocator]
-// static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+#[cfg(target_os = "linux")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 async fn ctrlc_channel() -> Result<mpsc::Receiver<()>, exitcode::ExitCode> {
     let (sender, receiver) = mpsc::channel(32);
