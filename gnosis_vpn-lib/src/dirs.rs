28b17b07a4409496261d2655b8a8dfd7deaed4f6
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-02-04 21:33:02 +0100

    removed feel good tests

--- gnosis_vpn-lib/src/dirs.rs
+++ gnosis_vpn-lib/src/dirs.rs
@@ -65,98 +65,11 @@ fn home() -> PathBuf {
 
 fn ensure_dir_with_owner(path: &PathBuf, uid: u32, gid: u32) -> Result<(), io::Error> {
     let res = DirBuilder::new().mode(0o700).create(path);
+    println!("res: {:?}", res);
     match res {
         Ok(()) => Ok(()),
         Err(e) if e.kind() == ErrorKind::AlreadyExists => Ok(()),
         Err(e) => Err(e),
     }?;
     unix_fs::chown(path, Some(uid), Some(gid))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::env;
-    use std::sync::Mutex;
-    use tempfile::tempdir;
-
-    static ENV_MUTEX: Mutex<()> = Mutex::new(());
-
-    fn with_env_var<F>(value: Option<&str>, test: F)
-    where
-        F: FnOnce(),
-    {
-        let key = ENV_VAR_HOME;
-        let _guard = ENV_MUTEX.lock().unwrap();
-        let original_value = env::var(key).ok();
-
-        match value {
-            Some(v) => unsafe { env::set_var(key, v) },
-            None => unsafe { env::remove_var(key) },
-        }
-
-        // Run the test
-        let result = std::panic::catch_unwind(std::panic::AssertUnwindSafe(test));
-
-        // Restore original value
-        match original_value {
-            Some(v) => unsafe { env::set_var(key, v) },
-            None => unsafe { env::remove_var(key) },
-        }
-
-        if let Err(err) = result {
-            std::panic::resume_unwind(err);
-        }
-    }
-
-    #[test]
-    fn test_custom_gnosisvpn_home() {
-        let temp_dir = tempdir().expect("failed to create temp dir");
-        let temp_path = temp_dir.path().to_path_buf();
-        let temp_path_str = temp_path.to_str().unwrap();
-
-        with_env_var(Some(temp_path_str), || {
-            let home = home();
-            assert_eq!(home, temp_path, "Home should match the custom environment variable");
-
-            let cache = cache_dir("test").unwrap();
-            assert!(cache.starts_with(&temp_path), "Cache dir should be under custom home");
-
-            let config = config_dir("test").unwrap();
-            assert!(config.starts_with(&temp_path), "Config dir should be under custom home");
-        });
-    }
-
-    #[test]
-    fn test_default_gnosisvpn_home_unset() {
-        with_env_var(None, || {
-            let home = home();
-
-            #[cfg(target_os = "macos")]
-            assert_eq!(home, PathBuf::from(DEFAULT_STATE_DIR_MACOS));
-
-            #[cfg(not(target_os = "macos"))]
-            assert_eq!(home, PathBuf::from(DEFAULT_STATE_DIR_LINUX));
-        });
-    }
-
-    #[test]
-    fn test_gnosisvpn_home_directories_created() {
-        let temp_dir = tempdir().expect("failed to create temp dir");
-        let temp_path = temp_dir.path().to_path_buf();
-        let temp_path_str = temp_path.to_str().unwrap();
-
-        with_env_var(Some(temp_path_str), || {
-            // Act
-            let _ = cache_dir("some_file").unwrap();
-            let _ = config_dir("some_file").unwrap();
-
-            // Assert
-            let cache_exists = temp_path.join(CACHE_DIRECTORY).is_dir();
-            let config_exists = temp_path.join(CONFIG_DIRECTORY).is_dir();
-
-            assert!(cache_exists, ".cache directory should be created");
-            assert!(config_exists, "config directory should be created");
-        });
-    }
 }
