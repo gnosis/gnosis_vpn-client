91b74aebe581958307558a4cc96fe4067ebd87ea
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-02-18 15:43:44 +0100

    adjust blokli configuration config

--- gnosis_vpn-lib/src/hopr/blokli_config.rs
+++ gnosis_vpn-lib/src/hopr/blokli_config.rs
@@ -5,16 +5,14 @@ use std::time::Duration;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct BlokliConfig {
-    pub tx_confirm_timeout: Duration,
-    pub connection_timeout: Duration,
+    pub connection_sync_timeout: Duration,
     pub sync_tolerance: usize,
 }
 
 impl From<BlokliConfig> for BlockchainConnectorConfig {
     fn from(config: BlokliConfig) -> Self {
         BlockchainConnectorConfig {
-            tx_confirm_timeout: config.tx_confirm_timeout,
-            connection_timeout: config.connection_timeout,
+            connection_sync_timeout: config.connection_sync_timeout,
             sync_tolerance: config.sync_tolerance,
         }
     }
