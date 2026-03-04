4c332c1724dc662174b09f20a792bccbec7f4039
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-03-04 11:03:44 +0100

    fixed tests

--- gnosis_vpn-root/src/routing/linux.rs
+++ gnosis_vpn-root/src/routing/linux.rs
@@ -1346,10 +1346,7 @@ mod tests {
                     listen_port: Some(51820),
                     allowed_ips: Some("0.0.0.0/0".into()),
                     force_private_key: None,
-                    dns: wireguard::DNSConfig {
-                        overwrite: false,
-                        servers: "".to_string(),
-                    },
+                    dns: None,
                 },
                 wireguard::KeyPair {
                     priv_key: "test_priv_key".into(),
