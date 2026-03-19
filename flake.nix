497b0e6f59ef964d917bd6f0965f053e93df4f7d
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-03-19 21:15:04 +0100

    fixed formatting

--- flake.nix
+++ flake.nix
@@ -30,15 +30,16 @@
   };
 
   outputs =
-    inputs@{ self
-    , flake-parts
-    , nixpkgs
-    , rust-overlay
-    , crane
-    , advisory-db
-    , treefmt-nix
-    , pre-commit
-    , ...
+    inputs@{
+      self,
+      flake-parts,
+      nixpkgs,
+      rust-overlay,
+      crane,
+      advisory-db,
+      treefmt-nix,
+      pre-commit,
+      ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ treefmt-nix.flakeModule ];
@@ -48,12 +49,13 @@
         "aarch64-darwin"
       ];
       perSystem =
-        { config
-        , self'
-        , inputs'
-        , lib
-        , system
-        , ...
+        {
+          config,
+          self',
+          inputs',
+          lib,
+          system,
+          ...
         }:
         let
           pkgs = (
