61bed67be837bfd3444fcc3dae6ec3c5d0efdb9c
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-03-19 21:00:02 +0100

    remove  remaining x86_64 intel build artifacts

--- flake.nix
+++ flake.nix
@@ -30,16 +30,15 @@
   };
 
   outputs =
-    inputs@{
-      self,
-      flake-parts,
-      nixpkgs,
-      rust-overlay,
-      crane,
-      advisory-db,
-      treefmt-nix,
-      pre-commit,
-      ...
+    inputs@{ self
+    , flake-parts
+    , nixpkgs
+    , rust-overlay
+    , crane
+    , advisory-db
+    , treefmt-nix
+    , pre-commit
+    , ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ treefmt-nix.flakeModule ];
@@ -47,16 +46,14 @@
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ];
       perSystem =
-        {
-          config,
-          self',
-          inputs',
-          lib,
-          system,
-          ...
+        { config
+        , self'
+        , inputs'
+        , lib
+        , system
+        , ...
         }:
         let
           pkgs = (
@@ -74,7 +71,6 @@
           systemTargets = {
             "x86_64-linux" = "x86_64-unknown-linux-musl";
             "aarch64-linux" = "aarch64-unknown-linux-musl";
-            "x86_64-darwin" = "x86_64-apple-darwin";
             "aarch64-darwin" = "aarch64-apple-darwin";
           };
 
@@ -85,7 +81,6 @@
           # Target-specific build arguments
           # Each target triple has its own compiler flags and build configuration.
           # - Linux targets use musl for static linking and mold for faster linking
-          # - Darwin targets use different profiles based on architecture (intelmac for x86_64)
           # - All targets enable crt-static for standalone binaries
           targetCrateArgs = {
             "x86_64-unknown-linux-musl" = {
@@ -137,11 +132,6 @@
 
               # poing pkg-config to static libraries
               PKG_CONFIG_PATH = "${staticPkgs.openssl.dev}/lib/pkgconfig:${staticPkgs.sqlite.dev}/lib/pkgconfig:${staticPkgs.libmnl}/lib/pkgconfig:${staticPkgs.libnftnl}/lib/pkgconfig";
-            };
-            "x86_64-apple-darwin" = {
-              CARGO_PROFILE = "intelmac";
-              # force libiconv from macos lib folder
-              CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static -C link-arg=-L/usr/lib -C link-arg=-liconv";
             };
             "aarch64-apple-darwin" = {
               CARGO_PROFILE = "release";
